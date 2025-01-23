64e3e9044ce9fceb9a88690eaac402b20b1ca2c4
son0307 <son03071@naver.com>
2025-01-23 21:36:40 +0900

    Javascript 코드 중복 제거

--- src/main/resources/static/script/index.js
+++ src/main/resources/static/script/index.js
@@ -1,6 +1,6 @@
 // 게시글 리스트 페이지 JS
 
-// 검색 버튼 클릭 이벤트
+// 검색 버튼 클릭 또는 키워드 입력 후 엔터 이벤트
 document.addEventListener('DOMContentLoaded', () => {
    const keywordInput = document.getElementById('keyword-input');
    const searchButton = document.getElementById('search-button');
@@ -30,23 +30,24 @@ function performSearch() {
     window.location.href = `http://localhost:8080/?keyword=${keyword}`;
 }
 
+// URL에서 페이지를 설정하고 이동하는 공통 함수
+function navigateToPage(pageNumber) {
+    const currentUrl = new URL(window.location.href);
+    currentUrl.searchParams.set('page', pageNumber);
+    window.location.href = currentUrl.toString();
+}
+
 // 이전 버튼 클릭 처리 함수
 function handlePreviousPage(currentPage) {
-    const currentUrl = new URL(window.location.href);
-    currentUrl.searchParams.set('page', currentPage - 1);
-    window.location.href = currentUrl.toString();
+    navigateToPage(currentPage - 1);
 }
 
 // 다음 버튼 클릭 처리 함수
 function handleNextPage(currentPage) {
-    const currentUrl = new URL(window.location.href);
-    currentUrl.searchParams.set('page', currentPage + 1);
-    window.location.href = currentUrl.toString();
+    navigateToPage(currentPage + 1);
 }
 
 // 페이지 번호 클릭 처리 함수
 function handlePageNumber(pageNumber) {
-    const currentUrl = new URL(window.location.href);
-    currentUrl.searchParams.set('page', pageNumber);
-    window.location.href = currentUrl.toString();
+    navigateToPage(pageNumber);
 }
